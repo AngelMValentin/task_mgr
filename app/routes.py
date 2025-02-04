4d38a37db2d0bacd4aa581499fd397ed3958d1d4
AngelMValentin <angelvalentinusmc@gmail.com>
2025-02-03 21:18:30 -0800

    first commit class 3

---
+++ app/routes.py
@@ -0,0 +1,51 @@
+from flask import (
+    Flask,
+    request
+)
+from app.database import task
+
+app = Flask(__name__)
+
+# ReST - Representation State Transfer
+# ReST is an architectural design pattern that helps design our APIs services
+
+@app.get("/")
+@app.get("/tasks")
+def get_all_tasks():
+    out = {
+        "tasks": task.scan(),
+        "ok": True
+    }
+    return out
+
+@app.get("/tasks/<int:pk>")
+def get_single_task(pk):
+    out = {
+        "task": task.select_by_id(pk),
+        "ok": True
+    }
+    return out
+
+@app.post("/tasks")
+
+
+@app.put("/tasks/<int:pk>")
+def update_task(pk):
+    task_data = request.json
+    task.update_by_id(task_data, pk)
+    return "", 204
+
+
+
+@app.delete("/tasks/<int:pk>")
+
+# youll also the request context object
+# you can retrieve the income json from the request body by using: request.json
+
+# """
+# {
+#     "name": "task name",
+#     "summary: "task summary",
+#     "description": "task description"
+# }
+# """ 
---
+++ setup.sql
@@ -0,0 +1,33 @@
+
+-- How to drop tables:
+-- DROP TABLE task;
+
+-- Create out table:
+CREATE TABLE IF NOT EXISTS task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(64),
+    summary VARCHAR(128),
+    description TEXT,
+    is_done BOOLEAN DEFAULT 0
+);
+
+INSERT INTO task (
+    name, 
+    summary,
+    description 
+) VALUES
+(
+    "Walk the dog",
+    "Take Fido out for a stroll",
+    "Make sure you go three laps around the park"
+),
+(
+    "Wash dishes",
+    "All the dishes must be washed before 10pm",
+    "Make sure to use the fancy new dish soap"
+),
+(
+    "Buy groceries",
+    "Go to the supermarket and buy groceries",
+    "We need: eggs, bacon and tomatoes"
+)
