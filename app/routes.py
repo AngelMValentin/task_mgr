b5e38fd719d826ef20fc6739ea120fdb623f5053
AngelMValentin <angelvalentinusmc@gmail.com>
2025-02-08 20:02:27 -0800

    class 3 assignment submit

--- app/routes.py
+++ app/routes.py
@@ -28,15 +28,20 @@ def get_single_task(pk):
 
 @app.post("/tasks")
 def create_task():
-    task_data = request.jsontask.create_task(task_data)
+    task_data = request.json 
+    task.create_task(task_data)
     return "", 204
 
 
 @app.put("/tasks/<int:pk>")
 def update_task(pk):
-    task_data = request.json
-    task.update_by_id(task_data, pk)
-    return "", 204
+    existing_task = task.select_by_id(pk)
+    if not existing_task:
+        return {"error": "Task not found"}, 404  
+    
+    task_data = request.json  
+    task.update_by_id(task_data, pk)  
+    return "", 204  
 
 
 
@@ -45,13 +50,3 @@ def delete_task(pk):
     task.delete_by_id(pk)
     return "", 204
 
-# youll also the request context object
-# you can retrieve the income json from the request body by using: request.json
-
-# """
-# {
-#     "name": "task name",
-#     "summary: "task summary",
-#     "description": "task description"
-# }
-# """ 
