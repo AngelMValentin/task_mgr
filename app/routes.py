afcd87b3ec6c574480f0ea7e56b12c28883baf83
AngelMValentin <angelvalentinusmc@gmail.com>
2025-02-15 12:10:14 -0800

    ...

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from flask import (
     request
 )
 from app.database import task
+import json
+import logging
 
 app = Flask(__name__)
 
@@ -35,12 +37,12 @@ def create_task():
 
 @app.route("/tasks/<int:pk>", methods=["POST", "PUT"])
 def edit_task(pk):
-    existing_task = task.select_by_id(pk)
-    if not existing_task:
-        return {"error": "Task not found"}, 404  
-
-    task_data = request.form 
-    task.update_by_id(task_data, pk)  
+    # existing_task = task.select_by_id(pk)
+    existing_task = get_single_task(pk)  
+    print(existing_task)
+    # task_data = json.dumps(request.form)
+    logging.debug(existing_task)
+    task.update_by_id(existing_task, pk)
     return {"message": "Task updated successfully"}, 200
 
 
