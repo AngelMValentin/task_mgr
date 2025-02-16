f146492156e3d8d07960f3e061c3558210c3f73c
AngelMValentin <angelvalentinusmc@gmail.com>
2025-02-15 17:29:29 -0800

    I should've watched the recording a long time ago

--- app/routes.py
+++ app/routes.py
@@ -35,15 +35,11 @@ def create_task():
     return "", 204
 
 
-@app.route("/tasks/<int:pk>", methods=["POST", "PUT"])
-def edit_task(pk):
-    # existing_task = task.select_by_id(pk)
-    existing_task = get_single_task(pk)  
-    print(existing_task)
-    # task_data = json.dumps(request.form)
-    logging.debug(existing_task)
-    task.update_by_id(existing_task, pk)
-    return {"message": "Task updated successfully"}, 200
+@app.put("/tasks/<int:pk>")
+def update_task(pk):
+    task_data = request.json
+    task.update_by_id(task_data, pk)
+    return "", 204
 
 
 
